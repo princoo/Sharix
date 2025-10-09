414ab7f38a03d916df3ff35835a24fe547cdb832
princoo <princeineza@gmail.com>
2025-10-09 23:47:52 +0200

    refactor: make navbar fixed

--- src/components/custom/navigation/navbar.tsx
+++ src/components/custom/navigation/navbar.tsx
@@ -5,7 +5,7 @@ import Logo from "@/components/shares/logo";
 
 export function Navbar() {
   return (
-    <nav className="border-b bg-white">
+    <nav className="border-b bg-white fixed top-0 w-full z-50">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
           <Link href="/" className="flex items-center gap-2">
